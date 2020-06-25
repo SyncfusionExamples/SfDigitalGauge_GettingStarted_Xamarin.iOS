40c68d71eeb27184b0a32b6e9784dad6d8354097
AzureAD\RamyaSoundarRajan <ramya.soundarrajan@syncfusion.com>
2020-06-25 15:13:42 +0530

    Added digital gaguge sample in iOS.

---
+++ Gauge_GettingStarted/ViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Foundation;
+using Syncfusion.SfGauge.iOS;
+using UIKit;
+
+namespace Gauge_GettingStarted
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            SFDigitalGauge digitalGauge = new SFDigitalGauge();
+            digitalGauge.Frame = new CoreGraphics.CGRect(0,50,this.View.Frame.Width,this.View.Frame.Height-50);
+            digitalGauge.CharacterHeight = 36;
+            digitalGauge.CharacterWidth = 18;
+            digitalGauge.CharacterType = SFDigitalGaugeCharacterType.SFDigitalGaugeCharacterTypeSegmentSeven;
+            digitalGauge.Value = (NSString)"Syncfusion";
+            this.View.AddSubview(digitalGauge);
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
